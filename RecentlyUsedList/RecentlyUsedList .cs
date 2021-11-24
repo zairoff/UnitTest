7d3282b96ee3231bd400a1484f9d8e73751a1c6a
zairoff <marufzoiroff@gmail.com>
2021-11-24 18:23:40 +0500

    implemented add functionality

--- RecentlyUsedList/RecentlyUsedList .cs
+++ RecentlyUsedList/RecentlyUsedList .cs
@@ -5,6 +5,18 @@ namespace RecentlyUsedList
 {
     public class List
     {
-        
+        private readonly LinkedList<string> _items;
+        private readonly int _count;
+
+        public List(int capacity)
+        {
+            _items = new LinkedList<string>();
+            _count = capacity;
+        }
+
+        public void Add(string input)
+        {
+            _items.AddFirst(input);
+        }
     }
 }
