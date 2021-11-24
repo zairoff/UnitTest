3853a83b9be21f7a8840b9f3b2629c4b5720dc02
zairoff <marufzoiroff@gmail.com>
2021-11-24 18:39:54 +0500

    implemented add functionality with edge cases

--- RecentlyUsedList/RecentlyUsedList .cs
+++ RecentlyUsedList/RecentlyUsedList .cs
@@ -6,17 +6,31 @@ namespace RecentlyUsedList
     public class List
     {
         private readonly LinkedList<string> _items;
-        private readonly int _count;
+        private readonly int _capacity;
+        private int _count = 0;
 
         public List(int capacity)
         {
             _items = new LinkedList<string>();
-            _count = capacity;
+            _capacity = capacity;
         }
 
         public void Add(string input)
         {
+            if (string.IsNullOrEmpty(input))
+                throw new ArgumentNullException();
+
+            if (_count == _capacity)
+                throw new OverflowException();
+
             _items.AddFirst(input);
+            _count++;
+        }
+
+        public void Print()
+        {
+            foreach(var item in _items)
+                Console.WriteLine(item);
         }
     }
 }
